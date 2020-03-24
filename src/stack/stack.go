4e540e452d4621d33486618e4ae46329d9a7c2c3
Sergey Gorovov <gor-serg@bk.ru>
2020-03-24 15:01:13 +0300

    added constructor. changed data type to empty interface

--- src/stack/stack.go
+++ src/stack/stack.go
@@ -6,35 +6,43 @@ import (
 
 // Stack is LIFO (last in, first out) data structure
 type Stack struct {
-	Data []float32
-	len  int
+	data []interface{}
+	size int
+}
+
+// NewStack is a constructor for the Stack
+func NewStack() *Stack {
+	s := new(Stack)
+	s.data = []interface{}{}
+	s.size = 0
+	return s
 }
 
 // Push appends x to Data
 // Time complexity O(1)
-func (s *Stack) Push(x float32) {
-	s.Data = append(s.Data, x)
-	s.len++
+func (s *Stack) Push(x interface{}) {
+	s.data = append(s.data, x)
+	s.size++
 }
 
 // Peek returns x from top of the stack
 // Time complexity O(1)
-func (s *Stack) Peek() (float32, error) {
-	if s.len == 0 {
+func (s *Stack) Peek() (interface{}, error) {
+	if s.size == 0 {
 		return 0, fmt.Errorf("Stack is empty")
 	}
-	return s.Data[len(s.Data)-1], nil
+	return s.data[len(s.data)-1], nil
 }
 
 // Pop removes and returns x from top of the stack
 // Time complexity O(1)
-func (s *Stack) Pop() (float32, error) {
-	if s.len == 0 {
+func (s *Stack) Pop() (interface{}, error) {
+	if s.size == 0 {
 		return 0, fmt.Errorf("Stack is empty")
 	}
-	x := s.Data[len(s.Data)-1]
-	s.Data = s.Data[:len(s.Data)-1]
-	s.len--
+	x := s.data[len(s.data)-1]
+	s.data = s.data[:len(s.data)-1]
+	s.size--
 	return x, nil
 }
 
@@ -42,9 +50,9 @@ func (s *Stack) Pop() (float32, error) {
 // This method starts the count of the position from 1.
 // Return -1 if data not in the stack.
 // Time complexity O(n)
-func (s *Stack) Search(x float32) int {
-	for i := 1; i <= len(s.Data); i++ {
-		if s.Data[len(s.Data)-i] == x {
+func (s *Stack) Search(x interface{}) int {
+	for i := 1; i <= len(s.data); i++ {
+		if s.data[len(s.data)-i] == x {
 			return i
 		}
 	}
@@ -54,25 +62,25 @@ func (s *Stack) Search(x float32) int {
 // Clear removes all data from the stack.
 // Time complexity O(1)
 func (s *Stack) Clear() {
-	s.Data = []float32{}
-	s.len = 0
+	s.data = []interface{}{}
+	s.size = 0
 }
 
 // IsEmpty returns false if the stack is empty else true.
 // Time complexity O(1)
 func (s *Stack) IsEmpty() bool {
-	if s.len == 0 {
+	if s.size == 0 {
 		return true
 	}
 	return false
 }
 
 func (s Stack) String() string {
-	return fmt.Sprint(s.Data)
+	return fmt.Sprint(s.data)
 }
 
-// Size returns size of the stack
+// GetSize returns size of the stack
 // Time complexity O(1)
-func (s *Stack) Size() int {
-	return s.len
+func (s *Stack) GetSize() int {
+	return s.size
 }
